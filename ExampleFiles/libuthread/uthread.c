29184b59971ccb6bb3b9ef7ed5edd8778b42e0fb
root <root@bull.ece.ucdavis.edu>
2017-01-29 21:07:38 -0800

    Added Example Files

---
+++ ExampleFiles/libuthread/uthread.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define _UTHREAD_PRIVATE
+#include "context.h"
+#include "preempt.h"
+#include "queue.h"
+#include "uthread.h"
+
+struct uthread_tcb {
+	/* TODO Phase 2 */
+};
+
+void uthread_yield(void)
+{
+	/* TODO Phase 2 */
+}
+
+int uthread_create(uthread_func_t func, void *arg)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_exit(void)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_block(void)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_unblock(struct uthread_tcb *uthread)
+{
+	/* TODO Phase 2 */
+}
+
+struct uthread_tcb *uthread_current(void)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_start(uthread_func_t start, void *arg)
+{
+	/* TODO Phase 2 */
+}
