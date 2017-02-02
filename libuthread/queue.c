a403d960c6f9e0c1c7de34128921b5d0d969acd2
root <root@bull.ece.ucdavis.edu>
2017-02-02 01:20:06 -0800

    Added queue_delete()

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -18,12 +18,12 @@ typedef struct queue {                                  /* Define queue FIFO str
 } queue;
 /* **************************************************** */
 /* **************************************************** */
-/*                     Queue Create                     */
+/*                    Queue Create                      */
 /* **************************************************** */
 queue_t queue_create(void)
 {
     queue_t Q = (queue*) malloc(sizeof(queue));         /* Allocate memory for the queue        */
-    if (Q == NULL) return Q;                            /* Return NULL pointer if malloc fails  */
+    if (Q == NULL) return Q;                            /* malloc failed, return NULL pointer   */
 
     Q->head = NULL;                                     /* Set the head                         */
     Q->tail = NULL;                                     /* Set the tail                         */
@@ -85,15 +85,45 @@ int queue_delete(queue_t queue, void *data)
     /* You cannot make any assumption on the *data that is given. */
     /* In other words, this data doesn't belong to the queue API */
 
+    /* Do we just delete the node that points to the data ?? */
+    /* Or do we also free the data??                    */
+
+    node *curr, *prev;                                  /* Current node to iterate over         */
+
     if (queue == NULL)          return -1;              /* Passed NULL queue, return fail       */
     if (queue->head == NULL)    return -1;              /* Queue head is NULL, return fail      */
     if (queue->tail == NULL)    return -1;              /* Queue tail is NULL, return fail      */
-    /* TODO Phase 1 */
 
+    curr = queue->head;                                 /* Current node is the head             */
+    prev = NULL;                                        /* Previous node                        */
 
+    while(curr->next != queue->head) {                  /* While more items in the queue        */
+        if (curr->data == data) {                       /* If data is found                     */
+            prev = curr->next;                          /* Store pointer to next node           */
+            curr->data = curr->next->data;              /* Overwrite data w/ next node data     */
+            curr->next = curr->next->next;              /* Overwrite next w/ next node next     */
+            free(prev);                                 /* Delete the next node                 */
+            if (curr->next == queue->head)              /* If node deleted was the tail         */
+                queue->tail = curr;                     /* Update the tail pointer              */
+            return 0;                                   /* Data was found, return success       */
+        }
+        prev = curr;                                    /* Save the node as the previous node   */
+        curr = curr->next;                              /* Iterate to next node                 */
+    }
 
-    return 0;                                           /* Return success                       */
+    if(curr->data == data) {                            /* Check last item in queue             */
+        if (curr == queue->head) {                      /* If its the head, only 1 item in Q    */
+            queue->head = NULL;                         /* Nullify the head                     */
+            queue->tail = NULL;                         /* Nullify the tail                     */
+        } else {                                        /* Otherwise, will delete current tail  */
+            queue->tail = prev;                         /* Update the queue's tail pointer      */
+            prev->next = queue->head;                   /* Set new tail->next to point to head  */
+        }
+        free(curr);                                     /* Delete the node                      */
+        return 0;                                       /* Data was found, return success       */
+    }
 
+    return -1;                                          /* Data not found, return fail          */
 }
 /* **************************************************** */
 /* **************************************************** */
@@ -102,7 +132,7 @@ int queue_delete(queue_t queue, void *data)
 int queue_iterate(queue_t queue, queue_func_t func)
 {
     if (queue == NULL)          return -1;              /* Passed NULL queue, return fail       */
-    if (func == NULL)           return -1;              /* Passed NULL func, return fail        */
+    if (func == NULL)           return -1;              /* Passed NULL function, return fail    */
 	/* TODO Phase 1 */
 
 
@@ -119,13 +149,13 @@ int queue_length(queue_t queue)
     node *curr;
 
     if (queue == NULL)          return -1;              /* Passed NULL queue, return fail       */
-    if (queue->head == NULL)    return  0;              /* Queue head is NULL, return 0         */
+    if (queue->head == NULL)    return  0;              /* Queue head is NULL, length is 0      */
     if (queue->tail == NULL)    return -1;              /* Q head not NULL, tail is NULL, fail  */
 
     curr = queue->head;                                 /* Current node to iterate over         */
 
-    while(curr->next != queue->head) {                  /* While more items in the list         */
-        curr = curr->next;                              /* Get the next node in the list        */
+    while(curr->next != queue->head) {                  /* While more items in the queue        */
+        curr = curr->next;                              /* Get the next node in the queue       */
         length++;                                       /* Increment the length                 */
         if (curr == NULL)       return -1;              /* Node improperly set, return fail     */
     }
@@ -133,4 +163,3 @@ int queue_length(queue_t queue)
     return length;                                      /* Return the length of the queue       */
 }
 /* **************************************************** */
-
