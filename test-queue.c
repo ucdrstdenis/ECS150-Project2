a0978ee871ffd046b2cf222999a1f9bbd0f2d02c
Robert StDenis <rstdenis@pc22.cs.ucdavis.edu>
2017-02-05 16:52:16 -0800

    Updated test-queue.c

--- test-queue.c
+++ test-queue.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <queue.h>
+#include <assert.h>
 
 queue_t myqueue;
 
@@ -13,15 +15,20 @@ void f()
 {
 	int a = 2;
 	int b = 3;
-	queue_enqueue(a);
-	queue_enqueue(b);
-	assert(queue_lenght(myqueue) == 2);
+	queue_enqueue(myqueue, &a);
+	queue_enqueue(myqueue, &b);
+	assert(queue_length(myqueue) == 2);
 
         queue_iterate(myqueue, mycustomfunc);
 
 	// after iterating, one element has been removed
-	assert(queue_lenght(myqueue) == 1);
+	assert(queue_length(myqueue) == 1);
+
+	assert(queue_destroy(NULL) == -1);
+	assert(queue_enqueue(NULL, NULL) == -1);
 }
+
+
 
 
 int main (void)
