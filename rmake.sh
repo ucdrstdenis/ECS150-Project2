12d9929f2bf2fb8b4a23146b3c9f7d1de23fa78b
root <root@bull.ece.ucdavis.edu>
2017-02-08 23:56:17 -0800

    Removed rmake.sh from .gitignore

---
+++ rmake.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Remote compile script. 
+# Copy project to CSIF and compile it
+# Only works on Linux ATM, MAC version soon
+
+#### Edit below
+
+# CSIF Username
+USERNAME='rstdenis'
+
+# CSIF computer	
+PC='pc46'
+
+# PATH of project on local machine
+# keep this script in project directory
+PROJECT_FROM=`pwd`
+
+# PATH of project on CSIF account, assume ~/ prefix	
+PROJECT_TO="ECS150-Project2"
+
+########################################################
+
+rsync_="$PROJECT_FROM $USERNAME@$PC.cs.ucdavis.edu:~/"
+ssh_="$USERNAME@$PC.cs.ucdavis.edu cd $PROJECT_TO; make clean; make"
+
+# Copy the project from host to CSIF client
+rsync -azvuhP --del $rsync_
+
+# SSH and compile
+ssh $ssh_
+
+
